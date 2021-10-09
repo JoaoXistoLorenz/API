d8f38ff421c3dc6f10fa631446fd123f595dab7f
JoaoVitorFrancoLorenz <jfrancolorenz@gmail.com>
2021-10-09 11:51:55 -0300

    Adicionando o Celebrate para o tratamento de erros e a verificação de parametros enviados pela requisição

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,63 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
 const productController = new ProductController();
 
 productsRouter.get('/', productController.index);
-productsRouter.get('/:id', productController.show);
-productsRouter.post('/', productController.create);
-productsRouter.put('/:id', productController.update);
-productsRouter.delete('/:id', productController.delete);
+
+/* Adicionando midlewere nas requisições */
+
+/* Recupera um produto */
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
+
+/* Adiciona um produto */
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
+
+/* Edita um produto */
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+);
+
+/* Deleta um produto */
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default productsRouter;
